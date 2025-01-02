246a0cb8684095a70f588313671f5c73acbcd7c7
JeongA Lee <201424077b@gmail.com>
2025-01-03 04:33:51 +0900

    [build] ComponentScan 추가

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -2,7 +2,9 @@ package kr.hhplus.be.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "kr.hhplus.be")
 @SpringBootApplication
 public class ServerApplication {
 
